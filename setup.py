9f6d432190de9e97eadd720320d99648418ab0f2
Liam Heisler <liam@liamMac.local>
2024-12-11 20:43:51 -0500

    setup changes

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     version=__VERSION__,
     url='https://github.com/liamheisler/package_test',
     author=__AUTHOR__,
-    py_modules=['package_test'],
+    packages=['package_test'],
     install_requires=['pandas', 'numpy']
 )
